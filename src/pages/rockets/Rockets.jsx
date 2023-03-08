85f4cb3a7890c40f5757594ee6dfed1dc6f684a4
Fene-87 <fenekayas87@gmail.com>
2023-03-08 13:29:04 +0300

    Fetch rocket data from SpaceX API

--- src/pages/rockets/Rockets.jsx
+++ src/pages/rockets/Rockets.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../../components/navbar/Navbar';
 import Rocket from '../../components/rocket/Rocket';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
 const Rockets = () => {
+  const { rocketList, status } = useSelector((store) => store.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [status, dispatch]);
+
   return (
     <div>
       <Navbar />
       <section>
-        <Rocket />
+        {rocketList.map((item) => (
+          <Rocket key={item.id} />
+        ))}
       </section>
     </div>
   );

db8ee7ee77e1c7d33a4f3e13e5a77b7c980ac7fc
Fene-87 <fenekayas87@gmail.com>
2023-03-10 14:12:16 +0300

    Resolve merge conflicts

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Navbar from '../../components/navbar/Navbar';
-<<<<<<< HEAD
-import RocketsProfile from '../../components/rocketsProfile/RocketsProfile';
-=======
 import { selectAllMissions } from '../../redux/missions/missionsSlice';
+import RocketsProfile from '../../components/rocketsProfile/RocketsProfile';
 import './profile.css';
->>>>>>> 71570c5f44938d0fe4ce6d2bbc4a84f1d1948e42
 
 const Profile = () => {
   const missions = useSelector(selectAllMissions);
@@ -24,22 +21,19 @@ const Profile = () => {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
-      <section>
+      <section className="profile-container">
+        <div className="profile-section">
+          <h2>My Missions</h2>
+          <div className="name-mission">
+            {joinedMissions.map((mission) => (
+              <div className="row" key={mission.id}>
+                <p>{mission.missionName}</p>
+              </div>
+            ))}
+          </div>
+        </div>
         <RocketsProfile />
       </section>
-=======
-      <div className="profile-section">
-        <h2>My Missions</h2>
-        <div className="name-mission">
-          {joinedMissions.map((mission) => (
-            <div className="row" key={mission.id}>
-              <p>{mission.missionName}</p>
-            </div>
-          ))}
-        </div>
-      </div>
->>>>>>> 71570c5f44938d0fe4ce6d2bbc4a84f1d1948e42
     </div>
   );
 };

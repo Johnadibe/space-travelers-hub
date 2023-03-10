aaa0757f818ef65810fad518d56b9066417137f9
Fene-87 <fenekayas87@gmail.com>
2023-03-10 13:20:43 +0300

    Implement styling to profile page

--- src/components/rocketsProfile/RocketsProfile.jsx
+++ src/components/rocketsProfile/RocketsProfile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './RocketsProfile.css';
 
 const RocketsProfile = () => {
   const { rocketList } = useSelector((store) => store.rockets);
